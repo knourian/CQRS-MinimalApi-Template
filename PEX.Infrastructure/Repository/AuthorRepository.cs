3b8a975d48d6cf9c0e9bfe046ecb0eff1aea211a
Kh.Nourian <kh.nourian@aaiccotec.com>
2022-06-22 14:29:46 +0430

    Added Author Repository

---
+++ PEX.Infrastructure/Repository/AuthorRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PEX.Domain.Model;
+using PEX.Infrastructure.Core;
+using PEX.Infrastructure.Core.Contracts;
+using PEX.Infrastructure.Repository.Contracts;
+
+namespace PEX.Infrastructure.Repository;
+public class AuthorRepository : BaseEFRepository<Author, int>, IAuthorRepository
+{
+    public AuthorRepository(IUnitOfWorks unitOfWorks, IServiceProvider serviceProvider) : base(unitOfWorks, serviceProvider)
+    {
+    }
+
+    public override IQueryable<Author> GetFullDbSet()
+    {
+        return GetDbSet();
+
+    }
+}
---
+++ PEX.Infrastructure/Repository/Contracts/IAuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PEX.Domain.Model;
+using PEX.Infrastructure.Core.Contracts;
+
+namespace PEX.Infrastructure.Repository.Contracts;
+public interface IAuthorRepository : IBaseRepository<Author, int>
+{
+
+}
