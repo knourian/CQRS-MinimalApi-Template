1b4a00819b5a6ef39fe94f4a80bd408277f0909e
Kh.Nourian <kh.nourian@aaiccotec.com>
2022-06-22 14:30:05 +0430

    Register Author Repository

--- PEX.Api/Extensions/ServiceCollectionExtensions.cs
+++ PEX.Api/Extensions/ServiceCollectionExtensions.cs
@@ -7,43 +7,51 @@ using Microsoft.OpenApi.Models;
 
 using PEX.Api.Contracts;
 using PEX.Application.Contracts;
+using PEX.Infrastructure.Core;
+using PEX.Infrastructure.Core.Contracts;
 using PEX.Infrastructure.Database;
+using PEX.Infrastructure.Repository;
+using PEX.Infrastructure.Repository.Contracts;
 
 namespace PEX.Api.Extensions;
 public static class ServiceCollectionExtensions
 {
-	public static IServiceCollection AddApplicationServices(this IServiceCollection services,
-		WebApplicationBuilder builder)
-	{
-		if (builder == null)
-		{
-			throw new ArgumentNullException(nameof(builder));
-		}
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        WebApplicationBuilder builder)
+    {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
 
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen(options =>
-		{
-			options.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
-			options.TagActionsBy(ta => new List<string> { ta.ActionDescriptor.DisplayName! });
-		});
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
+            options.TagActionsBy(ta => new List<string> { ta.ActionDescriptor.DisplayName! });
+        });
 
-		var connectionString = builder.Configuration.GetConnectionString("Default");
-		builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(connectionString));
-		builder.Services.AddMediatR(typeof(Program));
-		builder.Services.AddAutoMapper(typeof(Program));
-		builder.Services.AddAllModules(typeof(Program));
-		builder.Services.AddValidatorsFromAssemblyContaining(typeof(IAssemblyMarker));
+        var connectionString = builder.Configuration.GetConnectionString("Default");
+        builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(connectionString));
+        builder.Services.AddMediatR(typeof(Program));
+        builder.Services.AddAutoMapper(typeof(Program));
+        builder.Services.AddAllModules(typeof(Program));
+        builder.Services.AddValidatorsFromAssemblyContaining(typeof(IAssemblyMarker));
 
-		return services;
-	}
 
-	private static void AddAllModules(this IServiceCollection services, params Type[] types)
-	{
-		// Using the `Scrutor` to add all of the application's modules at once.
-		services.Scan(scan =>
-			scan.FromAssembliesOf(types)
-				.AddClasses(classes => classes.AssignableTo<IModule>())
-				.AsImplementedInterfaces()
-				.WithSingletonLifetime());
-	}
+        builder.Services.AddScoped<IUnitOfWorks, UnitOfWorks>();
+        builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+
+        return services;
+    }
+
+    private static void AddAllModules(this IServiceCollection services, params Type[] types)
+    {
+        // Using the `Scrutor` to add all of the application's modules at once.
+        services.Scan(scan =>
+            scan.FromAssembliesOf(types)
+                .AddClasses(classes => classes.AssignableTo<IModule>())
+                .AsImplementedInterfaces()
+                .WithSingletonLifetime());
+    }
 }
